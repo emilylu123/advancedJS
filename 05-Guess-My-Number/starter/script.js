03dea985c8e5103430417e3c2f02b7601f97d8d5
Yingyao Lu <emilylu123@gmail.com>
2021-04-09 23:53:20 +0930

    add guess number  game

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+let checkBtn = document.querySelector(".check");
+let againBtn = document.querySelector(".again");
+
+let score = 20;
+let highscore = 0;
+let secretNumber = Number(document.querySelector(".number").value);
+let guess;
+let msg = document.querySelector(".message")
+
+// random number generator 
+secretNumber = Math.floor(Math.random() * 20 + 1);
+
+const check = function () {
+    guess = Number(document.querySelector(".guess").value);
+    if (!guess) {
+        msg.textContent = "No Number!"
+        return;
+    }
+
+    console.log("Guess:", guess);
+    console.log("Number:", secretNumber);
+
+    if (guess === secretNumber) {
+        msg.textContent = "Correct Number!"
+        document.querySelector(".number").textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.color = 'red';
+
+        if (score > highscore) {
+            highscore = score
+            document.querySelector(".highscore").textContent = score
+        }
+    } else if (guess > secretNumber) {
+        msg.textContent = "Too high! Keep Guessing..."
+        score--;
+    } else {
+        msg.textContent = "Too low! Keep Guessing..."
+        score--;
+    }
+
+    if (score <= 0) {
+        msg.textContent = "You lost! Try Again!"
+        restore();
+    }
+
+    document.querySelector(".score").textContent = score;
+    console.log(document.querySelector(".score"), typeof document.querySelector(".score"));
+}
+
+const restore = function () {
+    score = 20;
+    secretNumber = Math.floor(Math.random() * 20 + 1);
+    document.querySelector(".number").textContent = '?'
+    msg.textContent = 'Start guessing...'
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".guess").value = ''
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('.number').style.color = 'black';
+
+
+}
+
+checkBtn.addEventListener('click', check);
+againBtn.addEventListener('click', restore);
+
+
+
+
