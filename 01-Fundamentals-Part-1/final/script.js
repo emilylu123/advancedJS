cce959197faaaad9cd83af214b794d5ec0074a66
Yingyao Lu <emilylu123@gmail.com>
2021-08-09 17:43:45 +0930

    update all sections

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -468,3 +468,11 @@ const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 */
+
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const result = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`;
+console.log(result)
+
+
